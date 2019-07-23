3f3f5e4c6c0386aa1e25013511a50ca2465cd2be
hamptoam <ameliabedelia94@gmail.com>
2019-07-23 15:53:40 -0500

    Initial binarytree commit, finished add looking into search.

---
+++ BinarySeachTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+   public class BinarySeachTree
+    {
+        public Node root;
+        public int count;
+        public int integer;
+        public Node add;
+
+
+        public BinarySeachTree()
+        {
+            root = null;
+            count = 0;
+            add = (node);
+        }
+
+
+     
+    
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        public int integer;
+        public int i;
+        public Node leftNode;
+        public Node rightNode;
+        public Node root;
+
+        public Node(int value)
+        {
+            integer = value;
+            i = value;
+            leftNode = null;
+            rightNode = null;
+
+
+        }
+        public Node Add(Node node, int data)
+        {
+            if (node == null)
+            {
+                node = new Node(data);
+            }
+
+            else if (node.integer > data)
+            {
+                Add(root.leftNode, i);
+            }
+            else if (node.integer < data)
+            {
+                Add(root.rightNode, i);
+            }
+
+            return root;
+        }
+    }
+}
+    
+
