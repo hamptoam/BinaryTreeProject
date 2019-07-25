c3b28b640fe2cc458f71b51f05a99f049c42dac3
hamptoam <ameliabedelia94@gmail.com>
2019-07-25 07:43:10 -0500

    Done, but having issues testing and might have to fix something but need to start Sweepstakes.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,19 @@ namespace BinaryTree
         static void Main(string[] args)
         {
             BinarySeachTree tree = new BinarySeachTree();
-            Node newNode = new Node();
-
+            Node newNode = new Node(30);
+            tree.Add(newNode);
+            Node secondNode = new Node(44);
+            newNode = secondNode;
+            tree.Add(secondNode);
+            Node thirdNode = new Node(38);
+            newNode = thirdNode;
+            tree.Add(thirdNode);
+            Node fourthNode = new Node(55);
+            newNode = fourthNode;
+            tree.Add(fourthNode);
+            Console.WriteLine(tree);
+            Console.ReadLine();
         }
     }
 }
